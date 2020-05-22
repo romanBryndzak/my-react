1d995f260bcce1089b2189957bb1dfe6a4422617
Roman <romabruzak@gmail.com>
2020-05-22 19:15:06 +0300

    Added componentDidUpdate for checking value prevProps.status with this.props.status.

--- src/Page/Portfolio/ProfileStatus/ProfileStatus.jsx
+++ src/Page/Portfolio/ProfileStatus/ProfileStatus.jsx
@@ -20,6 +20,12 @@ class ProfileStatus extends React.Component {
         this.setState({status: e.currentTarget.value})
     };
 
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.status !== this.props.status) {
+            this.setState({status: this.props.status})
+        }
+    }
+
     render() {
         return (
             <div>
