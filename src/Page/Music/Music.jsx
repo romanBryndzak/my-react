6671d7367edb9eb8832c30192d648d8cad5d473b
Roman <romabruzak@gmail.com>
2020-08-07 09:19:50 +0300

    Added content for News page.

--- src/Page/Music/Music.jsx
+++ src/Page/Music/Music.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Mu from "./Music.module.css"
 
 const Music = () => {
     return(
-        <div>
-            Music
+        <div className={Mu.container}>
+            <h2>Music</h2>
+            <div className={Mu.musicBox}>
+
+            </div>
         </div>
     )
 };
