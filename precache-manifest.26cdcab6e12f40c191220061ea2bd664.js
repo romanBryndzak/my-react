2dc6b6ab77acee78b414edd77904d330d7451c72
Roman <romabruzak@gmail.com>
2020-08-15 15:51:25 +0300

    Updates

---
+++ precache-manifest.26cdcab6e12f40c191220061ea2bd664.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f63a3df3b4734fd98dc31080969f60d2",
+    "url": "/my-react/index.html"
+  },
+  {
+    "revision": "52a26c6fd7474ece9b5f",
+    "url": "/my-react/static/css/4.560abbae.chunk.css"
+  },
+  {
+    "revision": "abb9405cd07f5986b4b5",
+    "url": "/my-react/static/css/main.a4b28925.chunk.css"
+  },
+  {
+    "revision": "76ffd1a9ce84c74c0d92",
+    "url": "/my-react/static/js/2.072ebde7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-react/static/js/2.072ebde7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c730009104bee2abe612",
+    "url": "/my-react/static/js/3.b3d49f65.chunk.js"
+  },
+  {
+    "revision": "52a26c6fd7474ece9b5f",
+    "url": "/my-react/static/js/4.6d36c375.chunk.js"
+  },
+  {
+    "revision": "cf338a7f2d331f478b1a",
+    "url": "/my-react/static/js/5.0167a46c.chunk.js"
+  },
+  {
+    "revision": "abb9405cd07f5986b4b5",
+    "url": "/my-react/static/js/main.20588e9a.chunk.js"
+  },
+  {
+    "revision": "1e0b5b4ff282d5955586",
+    "url": "/my-react/static/js/runtime-main.ce759930.js"
+  },
+  {
+    "revision": "88aeb098ee6cce86f0d07d638c282ed0",
+    "url": "/my-react/static/media/city.88aeb098.jpg"
+  },
+  {
+    "revision": "50aac17ae86738d6a0d1d04a3652ce39",
+    "url": "/my-react/static/media/preloader_64px.50aac17a.svg"
+  }
+]);
